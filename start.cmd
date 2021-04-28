919fa86c5a65cf7f393277e9f75d2d6439024332
Cxyder <82769970+Cxyder@users.noreply.github.com>
2021-04-28 17:07:13 +0200

    Add files via upload

--- start.cmd
+++ start.cmd
@@ -1,7 +1,22 @@
 @echo off
-echo Please make sure you have installed on your pc the last version of NodeJS before continuing...
-PAUSE
-echo Getting things ready...
+echo NodeJS Self-Testing...
+call npm -v> NUL
+call node -v> NUL
+if %ERRORLEVEL% == 9009 goto err
+goto start
+
+
+
+:err
+cls
+echo NodeJS Self-Test failure.
+echo Starting NodeJS reinstall procedure...
+cd Source/Dependencies
+timeout /t 2 /nobreak > NUL
+call nodejsreset.cmd
+
+:start
+echo NodeJS Self-Test ended, Launching app...
 cd Source
-call npminstaller.cmd
-node index
+call npm install discord.js > nul
+call node index
