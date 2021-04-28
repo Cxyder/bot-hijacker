9dfa9cb4336cedb3730e7162705a55015583236a
Cxyder <82769970+Cxyder@users.noreply.github.com>
2021-04-28 17:44:01 +0200

    Update BotHijacker Setup.cmd

--- BotHijacker Setup.cmd
+++ BotHijacker Setup.cmd
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZks0
-::ZQ05rAF9IBncCkqN+0xwdVsFAlTi
-::ZQ05rAF9IAHYFVzEqQISJw8UZA2IPWixA6d8
-::eg0/rx1wNQPfEVWB+kM9LVsJDCaNKCuSD78d6+P+/aq0p1kIRII=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFC5HSRa+E2W/IqcV/u3Hy++UqVkSRN4dd5vz076PI+sW+AXXcII4xjoJyskFGFVdZhfL
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 echo -----------------------//// Bot Hijacker Installer ////-----------------------
 echo Welcome to the Bot Hijacker installer, the installation should begin shortly.
