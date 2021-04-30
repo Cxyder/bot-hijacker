49579c51ffb023c80c837b00d35da3bbd3d9cc7f
Cxyder <82769970+Cxyder@users.noreply.github.com>
2021-04-30 23:29:27 +0200

    Add files via upload

--- Source/Dependencies/nodejsreset.cmd
+++ Source/Dependencies/nodejsreset.cmd
@@ -1,22 +1,43 @@
+@ECHO OFF
+REM BFCPEOPTIONSTART
+REM Advanced BAT to EXE Converter www.BatToExeConverter.com
+REM BFCPEEXE=
+REM BFCPEICON=
+REM BFCPEICONINDEX=-1
+REM BFCPEEMBEDDISPLAY=0
+REM BFCPEEMBEDDELETE=1
+REM BFCPEADMINEXE=0
+REM BFCPEINVISEXE=0
+REM BFCPEVERINCLUDE=0
+REM BFCPEVERVERSION=1.0.0.0
+REM BFCPEVERPRODUCT=Product Name
+REM BFCPEVERDESC=Product Description
+REM BFCPEVERCOMPANY=Your Company
+REM BFCPEVERCOPYRIGHT=Copyright Info
+REM BFCPEOPTIONEND
+@ECHO ON
 @echo off
-node -v > nul
-npm -v > nul
-if%ERRORLEVEL% == 9009 goto reset
+call node -v >nul 2 >nul
+call npm -v >nul 2 > nul
+if %ERRORLEVEL%==1 goto reset
+if %ERRORLEVEL%==0 goto end
 
 
 :reset
 cls
 echo Installing the NodeJS installer...
-curl -o nodejs-installer.msi https://nodejs.org/dist/v14.16.1/node-v14.16.1-x64.msi
+curl -s -o nodejs-installer.msi https://nodejs.org/dist/v14.16.1/node-v14.16.1-x64.msi
 goto installing
 
 :installing
+echo Please wait...
 start nodejs-installer.msi
+cls
 echo Please complete the NodeJS Setup, then press any button.
 pause > nul
-node -v > nul
-npm -v > nul
-if&ERRORLEVEL% == 9009 goto err
+call node -v >nul 2 >nul
+call npm -v >nul 2>nul
+if%ERRORLEVEL% == 1 goto err
 if%ERRORLEVEL% == 0 goto fixed
 
 
@@ -26,3 +47,7 @@ echo NodeJS Self-Test failure, NodeJS is not installed correctly, please refer t
 echo Press any button to try NodeJS Setup again - Press CTRL + C to close batch.
 pause > nul
 goto installing
+
+
+:end
+exit
